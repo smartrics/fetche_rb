6dc62e01f6be72903c32e2a3e5e5347d9ae4e42f
Fabrizio Cannizzo <smartrics@gmail.com>
2011-07-12 09:15:24 +0100

    removed ref to deleted class

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -1,5 +1,5 @@
 require File.dirname(__FILE__) + '/opt_parser.rb'
-require File.dirname(__FILE__) + '/deployment.rb'
+require File.dirname(__FILE__) + '/deployment_db.rb'
 require File.dirname(__FILE__) + '/fetcher_wui.rb'
 
 OptParser.parse(ARGV)
